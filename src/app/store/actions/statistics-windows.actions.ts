07c56793306823a74bbdce4440ea0d358cacf801
MP <47081376+mpiechulek@users.noreply.github.com>
2021-08-17 08:07:41 +0200

    added empyty actions class

--- src/app/store/actions/statistics-windows.actions.ts
+++ src/app/store/actions/statistics-windows.actions.ts
@@ -1,5 +1,5 @@
 import { Action } from "@ngrx/store";
-import {StatisticsWindows} from "../models/statistics-windows"
+import {StatisticsWindows} from "../models/statistics-windows";
 
 export enum StatisticsWindowActionTypes {
 
@@ -7,4 +7,8 @@ export enum StatisticsWindowActionTypes {
     CHOSE_TABLE= '[STATISTICS] Chose table section',
     CHOSE_STATS= '[STATISTICS] Chose stats section'
 
+}
+
+export class SwitchStatisticsSubsections implements Action {
+    
 }
