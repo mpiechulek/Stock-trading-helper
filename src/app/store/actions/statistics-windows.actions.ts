7f66e2565786f036b2e5e4f5b29c04716d5ac74e
MP <47081376+mpiechulek@users.noreply.github.com>
2021-08-17 07:59:28 +0200

    created enum

--- src/app/store/actions/statistics-windows.actions.ts
+++ src/app/store/actions/statistics-windows.actions.ts
@@ -2,5 +2,9 @@ import { Action } from "@ngrx/store";
 import {StatisticsWindows} from "../models/statistics-windows"
 
 export enum StatisticsWindowActionTypes {
-    
+
+    CHOSE_HARTS= '[STATISTICS] Chose harts section',
+    CHOSE_TABLE= '[STATISTICS] Chose table section',
+    CHOSE_STATS= '[STATISTICS] Chose stats section'
+
 }
